44ea0cee05ef7bc2a01b77aabc58d7185cd3cd04
3-rk <rohit.karve@gmail.com>
2019-05-05 08:45:05 +0530

    seleniumcuc integ with git

--- src/main/java/cucumberTest/TestRunner.java
+++ src/main/java/cucumberTest/TestRunner.java
@@ -1,14 +1,15 @@
 package cucumberTest;
 
-import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/main/Feature"
         ,glue={"java/cucumberTest/stepDefinition"}
         ,plugin = {"pretty", "html:target/cucumber"}
+        , dryRun = true
 
 
 )
